e12ab6a136f3ed662e9488fdc21e18927d33fcb7
Md Miraj Hossain <53442279+Ahmad-Miraz@users.noreply.github.com>
2022-09-10 09:39:15 +0000

    Initial commit

---
+++ simpleCalculator.js
@@ -0,0 +1,22 @@
+const prompt = require('prompt-sync')();
+let a = Number(prompt("Enter First Number: "));
+let b= Number(prompt("Enter Second Number: "));
+
+
+function add(a,b){
+  console.log(`The addition of ${a}+${b} = `,a+b)
+}
+function subtraction(a,b){
+  console.log(`The subtraction of ${a}-${b} = `,a-b);
+}
+function multiplication(a,b){
+  console.log(`The multiplication of ${a}*${b} = `,a*b);
+}
+function division(a,b){
+  console.log(`The division of ${a}/${b} = `,a/b);
+}
+
+add(a,b);
+subtraction(a,b);
+multiplication(a,b);
+division(a,b);
