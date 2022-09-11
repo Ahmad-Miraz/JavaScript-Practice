60434521a05755ee588af21b511768ce576576c9
Md Miraj Hossain <53442279+Ahmad-Miraz@users.noreply.github.com>
2022-09-11 18:40:30 +0000

    object in javascript

---
+++ object.js
@@ -0,0 +1,13 @@
+let student1={
+  name:"Miraj",
+  id:18101022,
+  dept:"cse",
+  batch:43,
+  occupation:"software engineer"
+}
+console.log(student1.name);
+console.log(student1.id);
+console.log(student1.dept);
+console.log(student1.batch);
+console.log(student1.occupation);
+console.log(student1);
