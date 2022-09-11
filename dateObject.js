991ed004ab6ab6a3c788aa8a12e4c2f38e994885
Md Miraj Hossain <53442279+Ahmad-Miraz@users.noreply.github.com>
2022-09-11 19:37:12 +0000

    dateObject

---
+++ dateObject.js
@@ -0,0 +1,14 @@
+let date=new Date();
+console.log(date)
+
+let year=date.getFullYear()
+console.log(year)
+
+let currentDate=date.getDate();
+console.log(currentDate);
+
+let day =date.getDay();
+console.log(day);
+
+let month=date.getMonth();
+console.log(month+1);
