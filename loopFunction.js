def550a349c986c01413262dd79dd0079ef8d095
Md Miraj Hossain <53442279+Ahmad-Miraz@users.noreply.github.com>
2022-10-03 04:18:32 +0000

    map and loop functions

---
+++ loopFunction.js
@@ -0,0 +1,8 @@
+let number=[4,3,6,5];
+for(let x in number){
+  console.log(number[x]);
+}
+console.log("This splits two loops")
+for(let y of number){
+  console.log(y);
+}
--- map.js
+++ map.js
@@ -1,8 +1,9 @@
-let number=[1,3,5,7,9];
-let squareNumber= number.map(function(x){
-  console.log(x*x);
+let number = [1, 3, 5, 7, 9];
+let squareNumber = number.map(function(x) {
+  console.log(x * x);
 });
 console.log("This line splits map and filter function");
-let number2=number.filter(function(x){
-  console.log(x*x);
-})
+let number2 = number.filter(function(x) {
+  return x < 5;
+});
+console.log(number2);
