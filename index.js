e81b4697b1348c1667ede533dd770078db7993e4
Md Miraj Hossain <53442279+Ahmad-Miraz@users.noreply.github.com>
2022-10-08 18:03:50 +0000

    Regular Expression test method

--- index.js
+++ index.js
@@ -1,20 +1,9 @@
-// console.log("Hello World");
-// const initialValue = 0;
-// const sum=(...args)=>{
-//   // const arg=[x,y,z];
-//   return args.reduce((previousValue,currentValue)=>previousValue+currentValue,initialValue)
-// }
-// let a=sum(1,2,3,4,5,6)
-// console.log(a);
+console.log("Hello World");
+const initialValue = 0;
+const sum=(...args)=>{
+  // const arg=[x,y,z];
+  return args.reduce((previousValue,currentValue)=>previousValue+currentValue,initialValue)
+}
+let a=sum(1,2,3,4,5,6)
+console.log(a);
 
-let a = [1,2,3,4,5];
-b=7;
-if(b>a.length){
-b=0;
-  for(b=0;b<=a.length;b++){
-    console.log(b)
-  }
-}
-else{
-  console.log(a);
-}
