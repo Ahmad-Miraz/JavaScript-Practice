70f576328074e9bd19f2aa1997f85275cd6adfdf
Md Miraj Hossain <53442279+Ahmad-Miraz@users.noreply.github.com>
2022-10-11 18:13:49 +0000

    another 2 dimensional array

--- passwordCheckerUsingRE.js
+++ passwordCheckerUsingRE.js
@@ -6,3 +6,23 @@
 let sampleWord = "astronaut22";
 let pwRegex = /(?=\w{5,})(?=\D+\d{2,})/; // Change this line
 console.log(pwRegex.test(sampleWord));
+
+// Fix the regex so that it checks for the names of Franklin Roosevelt or Eleanor Roosevelt in a case sensitive manner and it should make concessions for middle names.
+
+// Then fix the code so that the regex that you have created is checked against myString and either true or false is returned depending on whether the regex matches.
+let myString = "Eleanor Roosevelt";
+let myRegex = /(Eleanor|Franklin).*Roosevelt/; // Change this line
+console.log(myRegex.test(myString)); // Change this line
+// After passing the challenge experiment with myString and see how the grouping works
+
+// Write a regex fixRegex using three capture groups that will search for each word in the string one two three. Then update the replaceText variable to replace one two three with the string three two one and assign the result to the result variable. Make sure you are utilizing capture groups in the replacement string using the dollar sign ($) syntax.
+
+let str = "one two three";
+let fixRegex = /(\w+)\s(\w+)\s(\w+)/; // Change this line
+let replaceText = "$3 $2 $1"; // Change this line
+let result = str.replace(fixRegex, replaceText);
+
+// Write a regex and use the appropriate string methods to remove whitespace at the beginning and end of strings.
+let hello = "   Hello, World!  ";
+let wsRegex = /^\s+|\s+$/g; // Change this line
+let result = hello.replace(wsRegex, ""); // Change this line
